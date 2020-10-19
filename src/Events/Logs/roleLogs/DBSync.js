8f22e111a1957135a420b7ba5006241ca39a9add
krishna vison <heenathakkar95@gmail.com>
2020-10-19 18:28:14 +0530

    cleaning up

--- src/Events/Logs/roleLogs/DBSync.js
+++ src/Events/Logs/roleLogs/DBSync.js
@@ -12,7 +12,7 @@ module.exports = new baseEvent('ready',(client) => {
     for(let roles of rolesArray) rolesMap.set(roles.memberID,roles.roleIDs.split(','))
 
     for (let member of members.values()){
-        //console.log(member.user.username)
+        
         let roles = member.roles.cache.filter(r => !r.managed).keyArray()
         
         if(rolesMap.has(member.id)){
@@ -23,15 +23,17 @@ module.exports = new baseEvent('ready',(client) => {
             
             if(!(rolesAdded && rolesRemoved)) continue
             else {
+
                 db.prepare(`update roles set roleIDs = '${roles.join()}', position = ${member.roles.highest.rawPosition} where memberID = ${member.id}`).run()
+            
             }
         
         }
         else {
             newMembers = newMembers.concat(`,('${member.id}','${roles.join()}',${member.roles.highest.rawPosition})`)
         }
+
     }
-    //console.log(newMembers.substring(1))
     
     if(newMembers) db.prepare(`insert into roles (memberID,roleIDs,position) values ${newMembers.substring(1)}`).run()
 
