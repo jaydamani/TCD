4d9c6417ad39e1699d24bd91ef5087ff646a3e3b
jaydamani <jaydamani567@gmail.com>
2020-12-11 18:19:36 +0530

    autoMod now checks for swears as well

--- src/Events/autoMod.js
+++ src/Events/autoMod.js
@@ -2,6 +2,10 @@ const baseEvent = require("../registry/structures/baseEvent")
 const cooldown = require('../registry/structures/cooldown')
 const users = client.users.cache.keyArray()
 const ratelimits = config.ratelimits
+const Filter = require('bad-words')
+const filter = new Filter({ emptyList : false, placeholder : '*'})
+const { MessageEmbed } = require('discord.js')
+let { logs : { modLog } } = config
 
 const messageLimit = new cooldown('messages',users,ratelimits.message)
 const linkLimit = new cooldown('links',users,ratelimits.link)
@@ -9,18 +13,32 @@ const attachmentLimit = new cooldown('attachments',users,ratelimits.attachment)
 const mentionLimit = new cooldown('mentions',users,ratelimits.mention)
 const inviteLimit = new cooldown('invites' ,users,ratelimits.invite)
 
-module.exports =  new baseEvent('message',(message) => {
+module.exports =  new baseEvent('message',async (message) => {
 
     if(message.author.bot) return
     if(!message.member) return
     if(!message.member.manageable) return
     let text = message.content
     let argz = [message.author.id,message,message.member,message.guild]
-    
+ 
     messageLimit.add(...argz)
     if(text.match(/https?:\/\/(\w+.)+\w+/)) linkLimit.add(...argz)
     if(text.match(/discord.gg\/\w+ /)) inviteLimit.add(...argz)
     if(message.mentions.users.size) mentionLimit.add(...argz)
     if(message.attachments.size) attachmentLimit.add(...argz)
+    console.log(filter.isProfane(message.content))
+    if(filter.isProfane(message.content)){
 
+	modLog = client.channels.resolve(modLog)
+	message.delete()
+	let warnMessage = await message.channel.send(`${message.author}, please don't swear much. There are kids here like me :pleading_face:`)
+	modLog.send({ embed :new MessageEmbed({
+
+		title : `${message.author} sweared.`,
+		description : `He said "${message.content}"`,
+		url : warnMessage.url
+
+	})})
+
+    }
 })
