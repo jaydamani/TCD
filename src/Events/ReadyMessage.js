155a1acc28cf792cdacb9cea0b575193cc4e6be8
jaydamani <jaydamani567@gmail.com>
2020-12-13 18:23:38 +0530

    removed timeEngine

--- src/Events/ReadyMessage.js
+++ src/Events/ReadyMessage.js
@@ -1,9 +1,7 @@
 let baseEvent = require ("../registry/structures/baseEvent")
-const timeEngine = require('../registry/timeEngine')
 
 module.exports = new baseEvent("ready",() => {
 
-    timeEngine.run()
     console.log("ight, Imma start now")
 
 })
