296876b2e40e53e3f2009fabe7a5d82ca48cebd0
jaydamani <jaydamani567@gmail.com>
2020-12-13 18:24:16 +0530

    debugging

--- src/Events/xp/message.js
+++ src/Events/xp/message.js
@@ -1,20 +1,21 @@
 const baseEvent = require("../../registry/structures/baseEvent");
-const xpMap = client.xpMap
 const lastXPGiven = new Map()
 
 module.exports = new baseEvent('message',message => {
 
+    if(message.author.bot) return
     if(message.createdAt - lastXPGiven.get(message.author.id) < config.xp.coolDown/1000) return
 
     lastXPGiven.set(message.author.id, message.createdAt.getTime())
 
     //const channel = message.guild.channels.cache.get(config.xp.levelChannel) ?? message.channel
+    const xpMap = client.xpMap
     let xp = xpMap.get(message.author.id)
 
     if(!xp) xpMap.set(message.author.id,xp = { xp : 0, lvl : 1 })
     else xp.wasUpdated = true
 
-    xp.xp += (Math.random()*(config.xp.max - config.xp.min) + config.xp.min)
+    xp.xp += Math.floor(Math.random()*(config.xp.max - config.xp.min) + config.xp.min)
 
     if(xp.xp >= (result = 5*(xp.lvl**2) + 50*xp.lvl + 100)){
 
