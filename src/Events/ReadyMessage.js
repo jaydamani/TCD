f55ae0561b1c1c30ffd543a0a654f4a06466af4a
jaydamani <jaydamani567@gmail.com>
2020-12-15 17:53:49 +0530

    bot now sends a message on log in

--- src/Events/ReadyMessage.js
+++ src/Events/ReadyMessage.js
@@ -2,6 +2,7 @@ let baseEvent = require ("../registry/structures/baseEvent")
 
 module.exports = new baseEvent("ready",() => {
 
+    client.channels.cache.get('734686631675822151').send('logged in')
     console.log("ight, Imma start now")
 
 })
